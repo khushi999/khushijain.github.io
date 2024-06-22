f96bd7f4a8cb9512e8a698d05a08496744073818
Khushi Jain <83177168+khushi999@users.noreply.github.com>
2024-06-22 22:46:35 +0530

    personal portfolio

---
+++ src/components/AboutPage.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/spaceman.png'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+
+`
+const Spaceman = styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+img{
+    width: 100%;
+    height: auto;
+}
+`
+const Main =  styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+ backdrop-filter: blur(4px);
+  
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
+`
+
+
+
+
+const AboutPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+<Box>
+
+<LogoComponent theme='dark'/>
+<SocialIcons theme='dark'/>
+<PowerButton />
+<ParticleComponent theme='dark' />
+
+        <Spaceman>
+            <img src={astronaut} alt="spaceman" />
+        </Spaceman>    
+        <Main>
+        Hi there! 👋 I'm Khushi Jain, a passionate 3rd-year Computer Science student at York University, diving deep into the world of data engineering and software development. 
+<br /> <br/>
+When I'm not coding, you can find me working on cool projects.🚀 
+<br/> <br/>
+“Keep shining, keep grinding, and let your positivity light the way to success.” 🌟
+        </Main>
+
+        <BigTitle text="ABOUT" top="10%" left="5%" />
+
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default AboutPage
---
+++ src/data/WorkData.js
@@ -0,0 +1,41 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"NLP Chatbot",
+        description:"NLP powered customer service chatbot for streamlining query handling and reducing response times. ",
+        tags:["python","Dialogflow","FastAPI", "SQL"],
+        github:"https://github.com/khushi999/NLP-Chatbot-using-Dialogflow"
+    },
+    {
+        id:2,
+        name:"Web Scrapper",
+        description:"Python script to automate Amazon product data scraping, storage, and alerting for real-time trend analysis and price changes.",
+        tags:["python","Jupyter Notebooks"],
+        github:"https://github.com/khushi999/WScrapping"
+        
+    },
+    {
+        id:3,
+        name:"LBA Blogs",
+        description:"A tech blog website, inspired by the Github Blogs but easier to understand for students froma all the disciplines.",
+        tags:["React","Figma","HTML/CSS","TypeScript"],
+        github:"https://github.com/Lassonde-Blockchain-Association/lba-blog-v2"
+    },
+    {
+        id:4,
+        name:"Six degrees of Kevin Bacon",
+        description:"Six Degrees of Kevin Bacon game using graph algorithms to identify and display the shortest connection paths between actors through their shared movies.",
+        tags:["Neo4j","Java","Postman"],
+        github:"https://github.com/khushi999/Six_degrees_of_Kevin_Bacon"
+       
+    },{
+        id:5,
+        name:"Lyrics Search App",
+        description:"A responsive song lyrics search app using JavaScript and the lyrics.ovh API, enhancing user experience with a sleek, cross-device compatible interface.",
+        tags:["JavaScript","HTML","CSS","RestAPI"],
+        github:"https://github.com/khushi999/Lyrics-search-app"
+        
+    },
+] 
